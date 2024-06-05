ef579587ee9e90bcb678e8539fd276e56ef0a04e
Gastón <59783452+gaelceart@users.noreply.github.com>
2024-06-05 15:10:29 -0300

    FIX vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/notes-challenge/',
   plugins: [react()],
   preview: {
     host: '0.0.0.0',
